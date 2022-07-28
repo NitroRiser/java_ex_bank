bcec36dcc38c2cdc1ac558b0a28f23507afcd0cb
NitroRiser <jonathanfariastech@gmail.com>
2022-07-28 17:07:52 +0000

    Polimorfismo

--- src/application/Main.java
+++ src/application/Main.java
@@ -20,5 +20,14 @@ public class Main {
         Account acc3 = new BusinessAccount(1003, "Bob", 1000.00, 500.00);
         acc3.withdraw(200.00);
         System.out.println(acc3.getBalance());
+
+        Account x  = new Account(1020, "Alex", 1000.00);
+        Account y = new SavingsAccount(1023, "Maria", 1000.00, 0.01);
+
+        x.withdraw(200.0);
+        y.withdraw(200.0);
+
+        System.out.println(x.getBalance());
+        System.out.println(y.getBalance());
     }
 }
